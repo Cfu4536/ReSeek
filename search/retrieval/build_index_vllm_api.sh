c47d63618b2343e49c33ba4028257ec6de00c79e
cfu-exp <2778297606@qq.com>
2026-06-25 16:38:47 +0800

    init

--- search/retrieval/build_index_vllm_api.sh
+++ search/retrieval/build_index_vllm_api.sh
@@ -6,13 +6,13 @@
 # 2. 再运行此脚本构建索引
 
 # 配置参数
-CORPUS_PATH="/group/40077/shyuli/datasets/RL/hot_benchmark/wiki/hot-wiki-18.jsonl"         # 替换为你的语料库路径
-SAVE_DIR="/group/40077/shyuli/datasets/RL/hot_benchmark/wiki"
-RETRIEVAL_METHOD=qwen                 # 或者 bge, contriever 等
+CORPUS_PATH="/opt/datasets/TencentBAC/ReSeek-corpus/hot-wiki-18.jsonl"         # 替换为你的语料库路径
+SAVE_DIR="/opt/datasets/TencentBAC/ReSeek-corpus/"
+RETRIEVAL_METHOD=e5                 # 或者 bge, contriever 等
 BATCH_SIZE=64                          # API 调用的批次大小
 VLLM_API_URL="http://localhost:8000"   # vLLM 服务器地址
-#EMBEDDING_PATH="/group/40077/shyuli/datasets/RL/hot_benchmark/wiki/hot-wiki-18-e5/emb_e5.memmap" # 预计算的 embedding 文件路径
-corpus_file=/group/40077/shyuli/datasets/RL/hot_benchmark/wiki/hot-wiki-18.jsonl # jsonl
+# EMBEDDING_PATH="/group/40077/shyuli/datasets/RL/hot_benchmark/wiki/hot-wiki-18-e5/emb_e5.memmap" # 预计算的 embedding 文件路径
+# corpus_file=/group/40077/shyuli/datasets/RL/hot_benchmark/wiki/hot-wiki-18.jsonl # jsonl
 # change faiss_type to HNSW32/64/128 for ANN indexing
 # change retriever_name to bm25 for BM25 indexing
 
@@ -22,7 +22,7 @@ echo
 echo "开始构建索引..."
 
 # 运行索引构建
-python index_builder_api.py \
+python search/retrieval/index_builder_api.py \
     --retrieval_method $RETRIEVAL_METHOD \
     --corpus_path $CORPUS_PATH \
     --save_dir $SAVE_DIR \
--- search/retrieval/start_vllm_api.sh
+++ search/retrieval/start_vllm_api.sh
@@ -1,8 +1,10 @@
-CUDA_VISIBLE_DEVICES=3,4 vllm serve /opt/data/TencentBAC/e5-base-v2 \
+# export CUDA_DEVICE_ORDER='PCI_BUS_ID'
+
+CUDA_VISIBLE_DEVICES=3 vllm serve /opt/data/TencentBAC/e5-base-v2 \
   --task embed \
   --host 0.0.0.0 \
   --port 8000 \
-  --data-parallel-size 2 \
+  --data-parallel-size 1 \
   --gpu-memory-utilization 0.5
 
 
